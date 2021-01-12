c85bbff8a09720c3426783651cd6620230385411
MightyNiko <55553342+MuffinAmor@users.noreply.github.com>
2021-01-12 17:29:44 +0100

    first commit

---
+++ ip_adress/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;  
+using System.Linq;  
+using System.Text;  
+using System.Net; //Include this namespace 
+
+namespace ip_adress
+{
+     class Program{  
+        static void Main(string[] args)  
+        {   
+            string hostName = Dns.GetHostName();
+            Console.WriteLine(hostName);  
+            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();  
+            Console.WriteLine("My IP Address is :"+myIP);  
+            Console.ReadKey();  
+        }  
+    }  
+}
+ 
+   
+
+
