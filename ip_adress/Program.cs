7a91a24bb3c2aea643956e7a235a5ce2321194a6
MightyNiko <55553342+MuffinAmor@users.noreply.github.com>
2021-01-19 18:10:49 +0100

    local ip

--- ip_adress/Program.cs
+++ ip_adress/Program.cs
@@ -9,11 +9,16 @@ namespace ip_adress
      class Program{  
         static void Main(string[] args)  
         {   
-            string hostName = Dns.GetHostName();
-            Console.WriteLine(hostName);  
-            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();  
-            Console.WriteLine("My IP Address is :"+myIP);  
-            Console.ReadKey();  
+            //string hostName = Dns.GetHostName();
+            //Console.WriteLine(hostName);  
+            //string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();  
+            //Console.WriteLine("My IP Address is :"+myIP);  
+            //Console.ReadKey();  
+            string strHostName = System.Net.Dns.GetHostName();
+            IPHostEntry ipEntry = System.Net.Dns.GetHostEntry(strHostName);
+            IPAddress[] addr = ipEntry.AddressList;
+            string ip = addr[1].ToString();
+            Console.WriteLine(ip);
         }  
     }  
 }
