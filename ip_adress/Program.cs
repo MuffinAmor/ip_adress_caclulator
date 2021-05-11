8a6ba941be13efdd57270845109c476313709490
MightyNiko <55553342+MuffinAmor@users.noreply.github.com>
2021-05-11 16:31:44 +0200

    inital

--- ip_adress/Program.cs
+++ ip_adress/Program.cs
@@ -32,8 +32,9 @@ namespace ip_adress
 
                 dez = Convert.ToInt32(word);
                 bin = Convert.ToString(dez, 2);
-    
-                if (bin == "0"){
+
+                if (bin == "0")
+                {
                     bin = String.Concat(Enumerable.Repeat("0", 8));
                 }
 
@@ -44,23 +45,45 @@ namespace ip_adress
             double hosts = Math.Pow(2, count) - 2;
 
 
-
-
-
-            string firstIp = "192.168.40.1";
-
-
-            foreach (var i in hosts) //intschleife hier
+            static string IPAddrToBinary(string input)
             {
-                
+                return String.Join(".", ( // join segments
+                    input.Split('.').Select( // split segments into a string[]
+
+                        // take each element of array, name it "x",
+                        //   and return binary format string
+                        x => Convert.ToString(Int32.Parse(x), 2).PadLeft(8, '0')
+
+                        // convert the IEnumerable<string> to string[],
+                        // which is 2nd parameter of String.Join
+                    )).ToArray());
+            }
+
+            string[] numbers = ipadr.Split('.'); //Zerlegung der IP Adresse
+            string one = numbers[0];
+            string two = numbers[1];
+
+            string firstIp = one + "." + two + ".0.1";
+
+
+            string binary_string = Convert.ToString(IPAddrToBinary(firstIp));
+            Console.WriteLine(binary_string);
+
+            string[] tokens = binary_string.Split('.'); //Zerlegung des Binary Strings
+            int first = Convert.ToInt32(tokens[0]);
+            int second = Convert.ToInt32(tokens[1]);
+            int third = Convert.ToInt32(tokens[2]);
+            int fourth = Convert.ToInt32(tokens[3]);
+
+            for (int i = 0; i < hosts; i++)
+            {
+                Console.WriteLine(i);
             }
 
 
-
-
             Console.WriteLine($"Erste IP Adresse: {firstIp}");
-            Console.WriteLine($"Letzte IP Adresse: {last_ip}");
-            Console.WriteLine($"Broadcast Adresse: {broadcast}");
+            //Console.WriteLine($"Letzte IP Adresse: {last_ip}");
+            //Console.WriteLine($"Broadcast Adresse: {broadcast}");
             Console.WriteLine($"Anzahl Hosts: {hosts}");
         }
     }
