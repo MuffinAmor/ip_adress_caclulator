97eef3d498decf521cc4b2a84893f9b710524395
MightyNiko <55553342+MuffinAmor@users.noreply.github.com>
2021-06-15 17:49:19 +0200

    Network Ip, First IP  und Last IP

--- ip_adress/lib/AddressCalculation.cs
+++ ip_adress/lib/AddressCalculation.cs
@@ -8,6 +8,16 @@ namespace ip_adress.lib
 {
     public class AddressCalculation
     {
+        public string ConvertToIp(string Address)
+        {
+            var bytes = Address
+                .Split('.')
+                .Select(@byte => Convert.ToInt32(@byte, 2));
+            var address = IPAddress.Parse(string.Join(".", bytes));
+            Console.WriteLine(address);
+            return address.ToString();
+        }
+
         public string IPAddrToBinary(string input)
         {
             return String.Join(".", ( // join segments
@@ -22,9 +32,29 @@ namespace ip_adress.lib
                 )).ToArray());
         }
 
-        public string Calc_NetMask(string ip_address)
+        public string Calc_Work_String(string ip_address, string subnetz)
         {
-            return "";
+            var binString = IPAddrToBinary(ip_address);
+            var source = IPAddrToBinary(subnetz);
+            var count = source.Count(f => f == '1');
+            var net = binString.Replace(".", "").Substring(0, count);
+            var counter = 0;
+            var lastAddress = "";
+
+            foreach (var i in net)
+            {
+                counter += 1;
+                if (counter % 9 == 0 & i != 0)
+                {
+                    lastAddress += ".";
+                }
+                else
+                {
+                    lastAddress += i;
+                }
+            }
+
+            return lastAddress;
         }
 
         public double Calc_Host(string subnet)
@@ -53,6 +83,100 @@ namespace ip_adress.lib
             var hosts = Math.Pow(2, count) - 2;
 
             return hosts;
+        }
+
+        public string Calc_Last(string ip_address, string subnetz)
+        {
+            var workAddress = Calc_Work_String(ip_address, subnetz);
+            var addressLength = workAddress.Length;
+            var lastAddress = workAddress;
+
+            for (var i = addressLength; i < 35; i++)
+            {
+                if (i % 9 == 0 & i != 0)
+                {
+                    lastAddress += ".";
+                }
+
+                if (i < 34)
+                {
+                    lastAddress += "1";
+                }
+                else
+                {
+                    lastAddress += "0";
+                }
+            }
+
+            return lastAddress;
+        }
+
+        public string Calc_First(string ip_address, string subnetz)
+        {
+            var workAddress = Calc_Work_String(ip_address, subnetz);
+            var addressLength = workAddress.Length;
+            var firstAddress = workAddress;
+
+            for (var i = addressLength; i < 35; i++)
+            {
+                if (i % 9 == 0 & i != 0)
+                {
+                    firstAddress += ".";
+                }
+
+                if (i < 34)
+                {
+                    firstAddress += "0";
+                }
+                else
+                {
+                    firstAddress += "1";
+                }
+            }
+
+            return firstAddress;
+        }
+
+        public string Calc_Broad(string ip_address, string subnetz)
+        {
+            var workAddress = Calc_Work_String(ip_address, subnetz);
+            var addressLength = workAddress.Length;
+            var broadAddress = workAddress;
+
+            for (var i = addressLength; i < 36; i++)
+            {
+                if (i % 9 == 0 & i != 0)
+                {
+                    broadAddress += ".";
+                }
+                else
+                {
+                    broadAddress += "1";
+                }
+            }
+
+            return broadAddress;
+        }
+
+        public string Calc_Net(string ip_address, string subnetz)
+        {
+            var workAddress = Calc_Work_String(ip_address, subnetz);
+            var addressLength = workAddress.Length;
+            var netAddress = workAddress;
+
+            for (var i = addressLength; i < 35; i++)
+            {
+                if (i % 9 == 0 & i != 0)
+                {
+                    netAddress += ".";
+                }
+                else
+                {
+                    netAddress += "0";
+                }
+            }
+
+            return netAddress;
         }
     }
 }
